d378c866d524cbb79dec1ebcf9c560086a23732d
Mathieu-Md <matysallanet@gmail.com>
2025-04-28 14:52:35 +0200

    modif, création et rectification

--- src/main/java/bts/sio/webapp/repository/SportProxy.java
+++ src/main/java/bts/sio/webapp/repository/SportProxy.java
@@ -2,6 +2,7 @@ package bts.sio.webapp.repository;
 
 
 import bts.sio.webapp.CustomProperties;
+import bts.sio.webapp.model.Pays;
 import bts.sio.webapp.model.Sport;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,27 @@ public class SportProxy {
         );
 
         log.debug("Get Sports call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+    /**
+     * Get all sports
+     * @return An iterable of all sports
+     */
+    public Iterable<Sport> getLesSports() {
+
+        String baseApiUrl = props.getApiUrl();
+        String getSportUrl = baseApiUrl + "/sports";
+        System.out.println("url=" + getSportUrl);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Iterable<Sport>> response = restTemplate.exchange(
+                getSportUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Iterable<Sport>>() {}
+        );
+
+        log.debug("Get Sport call " + response.getStatusCode().toString());
 
         return response.getBody();
     }
